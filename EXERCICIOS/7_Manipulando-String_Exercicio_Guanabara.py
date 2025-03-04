40ddae7e75d77611b9019f3c49d4535ccc279d18
Anady Carvalho <anadyads2022@hotmail.com>
2025-03-04 19:16:08 -0300

    Finalizando_Exercicios_Strings

---
+++ EXERCICIOS/7_Manipulando-String_Exercicio_Guanabara.py
@@ -0,0 +1,52 @@
+#exercicio 1
+
+# nome = str(input('Qual o seu nome completo?  \n')).strip()
+# nome_maiusculo = nome.upper()
+# nome_minusculo = nome.lower()
+# letra_primeira = nome.capitalize()
+# primeiro_nome = len(nome) - nome.count(' ')
+# print(nome_maiusculo)
+# print(nome_minusculo)
+# print(primeiro_nome)
+# print(letra_primeira)
+# nome_contagem =nome.count('a', 1, 4) # intervalo
+# print(nome_contagem)
+
+# palavra = 'joao Carvalho'.strip()
+# print(palavra.find('Carvalho'))
+
+#EXERCICIO 2
+
+# num = int(input('Insira um de número de 0 a 9999:\n'))
+# if 0 <= num <= 9999:
+#   conversao = str(num).zfill(4)
+#   print(f'Milhar {conversao[3]}')
+#   print(f'Centena {conversao[2]}')
+#   print(f'Dezena {conversao[1]}')
+#   print(f'Unidade {conversao[0]}')
+# else:
+#   print("Número fora do intervalo válido. Por favor, insira um número entre 0 e 9999.")
+
+#EXERCICIO 4
+
+# cidade = str(input('Qual o nome da sua cidade ? \n')).strip()
+# print(cidade[:5].upper() == 'SANTO')
+
+#EXERCICIO 5
+# nome = str(input('Qual o seu nome completo ? \n')).strip()
+# print(f'{'silva' in nome.lower()}')
+
+#EXERCICIO 6
+# nome = str(input('Qual o seu nome completo ? \n')).strip()
+
+# print(f'A letra A aparece {nome.count('a')}')
+# print(f'A letra A Aparece pela primeira vez na posição {nome.find('a')}.')
+# print(f'Em que posição ela aparece na última vez {nome.rfind('a')}.')
+
+
+
+
+
+
+
+
