99e47f97c33705fcbb2a244a014f5c6e9692a72e
Anady Carvalho <anadyads2022@hotmail.com>
2025-03-05 12:54:47 -0300

    Iniciando_exercicios_CondicoesAninhadas

---
+++ EXERCICIOS/9_CondicoesAninhadas_Exercicios_GUanabara.py
@@ -0,0 +1,34 @@
+#EXERCICIO 1
+
+# valor_casa = float(input('Qual o valor da casa?\n'))
+# salario = float(input('Qual o valor do seu salário?\n'))
+# anos = int(input('Quantos anos vc deseja financiar?\n'))
+# porcentagem = salario * (30 / 100)
+# anos1 = anos * 12
+# financiamento = valor_casa / anos1
+# if financiamento < porcentagem:
+#   print(f'Empréstimo Autorizado, dentro da margem dos 30% - {porcentagem: .2f}, valor da prestação por mês {financiamento: .2f}.')
+# else:
+#   print(f'Uma pena, empréstimo negado, ultrapassa o valor dos 30% - {porcentagem: .2f}, valor da prestação por mês {financiamento: .2f}.')
+
+#EXERCICIO 2
+
+# num = int(input('Digite um número inteiro?\n'))
+# print('ESCOLHA UMAS DAS BASES PARA CONVERSÃO:')
+# print('[1] Converter para BINÁRIO')
+# print('[2] Converter para OCTAL')
+# print('[3] Converter para HEXADECIMAL')
+# opcao = int(input('Digite sua opção?\n'))
+
+# if opcao == 1:
+#   print(f'O {num} em binário é {bin(num)[2:]}.')
+#   # A função bin() retorna uma string que começa com '0b', então [2:] é usado para remover os dois primeiros caracteres e exibir apenas a parte binária.
+# elif opcao == 2:
+#   print(f'O {num} em octal é {oct(num)[2:]}.')
+#   #A função oct() retorna uma string que começa com '0o', então [2:] é usado para remover os dois primeiros caracteres e exibir apenas a parte octal.
+# elif opcao == 3:
+#   print(f'O {num} em hexadecimal é {hex(num)[2:0]}.')
+#   #A função hex() retorna uma string que começa com '0x', então [2:] é usado para remover os dois primeiros caracteres e exibir apenas a parte hexadecimal.
+# else:
+#   print('Opção inválida, tente novamente')
+
