c8bf8edb5692bcd54d67176d752e91ba9f329a65
Anady Carvalho <anadyads2022@hotmail.com>
2025-03-30 22:18:33 -0300

    Finalizando_Teoria_FuncaoLambda

---
+++ AULAS/20_funcoes_lambda_anonimas.py
@@ -0,0 +1,26 @@
+# def dobrar(n):
+#   return n *2
+
+# print(f'FUNÇÃO NORMAL: {dobrar(5)}')
+
+# # função LAMBDA
+
+# teste_lambda = lambda x: x * 2
+# print(f'FUNÇÃO LAMBDA: {teste_lambda(5)}')
+
+# def tipo_numero(n):
+#   if n < 0:
+#     return "negativo"
+#   elif n == 0:
+#     return 'zero'
+#   else:
+#     return "positivo"
+
+# print(tipo_numero(5))
+  
+# teste_lambda = lambda n: 'negativo' if n < 0 else ("ZERO" if n == 0 else "positivo")
+# print(teste_lambda(-2))
+
+pessoas = [('João', 35), ('Allan', 25),('Pedro', 40)]
+ordem_pessoas = sorted(pessoas, key=lambda x: x[0])
+print(ordem_pessoas)
