479056b0656e23d5c6564886b679d50e9d22a581
Anady Carvalho <anadyads2022@hotmail.com>
2025-04-05 17:17:55 -0300

    Iteracoes_Max_Min_Sun-Listas

---
+++ AULAS/27_max_min_sun_len.py
@@ -0,0 +1,9 @@
+lista = [58.5, 63.3, 71.3, 69.4, 68.2]
+# soma = sum(lista)
+# tamanho = len(lista)
+# media = soma / tamanho
+maior = max(lista)
+menor = min(lista)
+# print(f"A média é {media}")
+print(maior)
+print(menor)  
---
+++ AULAS/28_Iteracao_Listas.py
@@ -0,0 +1,10 @@
+nomes = ['Alice', 'Bruno', 'Clara', 'Daniel', 'Eduarda']
+# for indice, nomes in enumerate(nomes):
+#   print(f'O indice do aluno {nomes} é {indice}.')  
+# for nome in nomes:
+#   print(nome)
+nota = [85, 90, 78, 92, 88]
+
+for indice, nome in enumerate (nomes):
+
+    print(f'A nota do aluno {nome} com índice {indice +1} é {nota[indice]}.')
